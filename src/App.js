fdf85d05c016d3485783283316953cccb9e2ee69
crystal11111 <crystal031230@gmail.com>
2024-03-13 07:05:16 -0600

    update projects

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   useEffect(() => {
