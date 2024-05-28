fe17663136e3b4586a53053e790ba91ae2dd116f
crystal11111 <crystal031230@gmail.com>
2024-05-28 15:12:02 -0600

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   useEffect(() => {
