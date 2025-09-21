c741c803e850a436656139be757aa8dec8ccba3d
crystal11111 <crystal031230@gmail.com>
2025-09-21 02:19:48 -0600

    fix contact form and update branding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import logo from './assets/img/crystal_gem.png';
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar, Banner, Skills, Projects, Contact, Footer } from "./components";
 
 const App = () => {
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => {console.log(data)})
-  }, [])
 
   return (
     <div className="App">
